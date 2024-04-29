13ee92902fee5ad4cd5b7bdc9fa3182813c46ce2
yago-codev <yagomilano92@gmail.com>
2024-04-29 20:57:18 -0300

    configurando theme provider e declaration merge para fornecer a tipagem do tema

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,6 @@
 'use client'
 
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   @font-face {
@@ -35,17 +35,17 @@ const GlobalStyles = createGlobalStyle`
     -moz-osx-font-smoothing: grayscale;
   }
 
-  html {
-    font-size: 62.5%; // 1rem = 10px
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%; // 1rem = 10px
+    }
 
-  html, body, #__next {
-    height: 100%;
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
 
-  body {
-    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
 `
 
 export default GlobalStyles
