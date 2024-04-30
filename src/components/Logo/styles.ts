f6a2f256ec49813506b464d4d8fcdb10f9f0aa33
yago-codev <yagomilano92@gmail.com>
2024-04-30 14:08:51 -0300

    finalizando os testes de media queries do componente Logo

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,9 +1,38 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 import { LogoProps } from '.'
 
+const wrapperModifiers = {
+  normal: () => css`
+    width: 11rem;
+    height: 3.3rem;
+  `,
+  large: () => css`
+    width: 20rem;
+    height: 5.9rem;
+  `,
+  hideOnMobile: () => css`
+    // quando for menor que 768...
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
+  `
+}
+
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color}) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
+    ${!!size && wrapperModifiers[size!]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile}
   `}
 `
--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import 'jest-styled-components'
 
 import Logo from '.'
 import { renderWithTheme } from 'utils/tests/helpers'
@@ -23,5 +24,36 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
       color: '#030517'
     })
+  })
+
+  // deve renderizar um logo grande
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  // deve renderizar um logo normal quando size for default
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo size="normal" />)
+
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  // deve renderizar um logo grande sem texto se hideOnMobile for true
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
