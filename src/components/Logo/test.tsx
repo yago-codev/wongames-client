0629ed2daffc6ebbfd001859fe7e7fd8397e00b6
yago-codev <yagomilano92@gmail.com>
2024-04-30 09:58:43 -0300

    criando helper para testes com tema e adicionando o primeiro teste para o componente Logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Logo from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  // deve renderizar um label branco por padrão
+  it('should render a white label by default', () => {
+    // renderizar o componente `render`
+    renderWithTheme(<Logo />)
 
-    expect(screen.getByText(/Logo/i)).toBeInTheDocument()
+    // selecionar o elemento testado `screen`
+    // expect - assertion - asserção - comparação - análise
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  // deve renderizar um label preto quando a cor for passada por parâmetro
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
