e256f2b4f63e87fff2977c307d8dcd1a9489da49
yago-codev <yagomilano92@gmail.com>
2024-04-29 22:05:31 -0300

    criando componente Logo

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import Logo from '.'
+
+describe('<Logo />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Logo />)
+
+    expect(screen.getByText(/Logo/i)).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
