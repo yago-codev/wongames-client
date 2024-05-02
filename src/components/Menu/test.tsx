83e4413c90634eb4933b207b488da2769cb24538
yago-codev <yagomilano92@gmail.com>
2024-05-02 16:40:19 -0300

    criando comportamento de abrir/fechar o componente Menu

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,11 +1,38 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 
 import Menu from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 describe('<Menu />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Menu />)
+  // deve renderizar o menu
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
 
-    expect(screen.getByText(/Menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  // deve abrir/fechar o menu mobile
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    // selecionar o menu
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // verificar se o menu está escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // clicar no botão de abrir e verificar se o menu abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // clicar no botão de fechar e verificar se o menu fechou
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
