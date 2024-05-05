a8191794a96239d179b3f168fbc319b14ccc1fcf
yago-codev <yagomilano92@gmail.com>
2024-05-05 01:27:56 -0300

    ajustando estilização de acordo com a prop as

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,4 @@
 import styled, { css, DefaultTheme } from 'styled-components'
-
 import { ButtonProps } from '.'
 
 type WrapperProps = {
@@ -11,13 +10,11 @@ const wrapperModifiers = {
     height: 3rem;
     font-size: ${theme.font.sizes.xsmall};
   `,
-
   medium: (theme: DefaultTheme) => css`
     height: 4rem;
     font-size: ${theme.font.sizes.small};
     padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
   `,
-
   large: (theme: DefaultTheme) => css`
     height: 5rem;
     font-size: ${theme.font.sizes.medium};
@@ -27,10 +24,6 @@ const wrapperModifiers = {
     width: 100%;
   `,
   withIcon: (theme: DefaultTheme) => css`
-    display: 'inline-flex';
-    align-items: center;
-    justify-content: center;
-
     svg {
       width: 1.5rem;
 
@@ -43,20 +36,23 @@ const wrapperModifiers = {
 
 export const Wrapper = styled.button<WrapperProps>`
   ${({ theme, size, fullWidth, hasIcon }) => css`
-    width: ${fullWidth ? '100%' : 'auto'};
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
     cursor: pointer;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
+    text-decoration: none;
 
     &:hover {
-      background-color: liner-gradient(180deg, #e35565 0%, #d958a6 50%);
+      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
     }
 
     ${!!size && wrapperModifiers[size](theme)};
-    ${!!fullWidth && wrapperModifiers['fullWidth']()};
-    ${!!hasIcon && wrapperModifiers['withIcon'](theme)};
+    ${!!fullWidth && wrapperModifiers.fullWidth()};
+    ${!!hasIcon && wrapperModifiers.withIcon(theme)};
   `}
 `
